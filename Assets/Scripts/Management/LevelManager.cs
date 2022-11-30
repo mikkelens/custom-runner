e13647b84fabe0e2bc4fcb59ce0a0f3b26eccb38
Mikkel <Mikkel123sej@gmail.com>
2022-11-30 14:14:25 +0100

    added level settings and updated tools

--- Assets/Scripts/Management/LevelManager.cs
+++ Assets/Scripts/Management/LevelManager.cs
@@ -1,9 +1,31 @@
+using Sirenix.OdinInspector;
 using Tools.Types;
+using UnityEngine;
 
 namespace Management
 {
+	/// <summary>
+	/// Spawns obstacles in the way of the player, and despawns them when they move outside the camera again.
+	/// </summary>
 	public class LevelManager : Singleton<LevelManager>
 	{
+		[SerializeField, Required] private LevelSettings settings;
 
+		private Camera _cam;
+		private Camera Cam
+		{
+			get
+			{
+				if (_cam != null) return _cam;
+				return _cam = Camera.main;
+			}
+		}
+		private float TotalSpawnHeight => Cam.orthographicSize;
+		private float SpawnDistance => Cam.orthographicSize * Cam.aspect;
+
+		private void FixedUpdate()
+		{
+
+		}
 	}
 }
